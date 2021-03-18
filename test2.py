1af412ece3e08fd2fe0788b9a3e18d69b0a7ea10
dasuntheekshanagit <72508142+dasuntheekshanagit@users.noreply.github.com>
2021-03-18 20:13:13 +0530

    choose closest path
    add a funtion to chose close path form target position. (should be improved) funtion fail when come visited position.

--- test2.py
+++ test2.py
@@ -1,3 +1,5 @@
+import math
+
 def next_position(m,i,j,d):
 	if not m[j-1][i-1] == 1:
 		m[j-1][i-1]=5
@@ -19,28 +21,70 @@ def next_position(m,i,j,d):
 	else:
 		m[j-1][i-1]=5
 		#print("{} {} {}".format(i,j,d))
-		return i,j
+		return i,j,d
 
 def all_posi(m,i,j):
-	print("{} {}".format(i,j))
+	#print("{} {}".format(i,j))
 	answer = []
 	for x in range(4):
-		posi_n = next_position(m,i,j,x)
+		posi_n = next_position(m,i,j,x+1)
 		if not posi_n == -1:
 			answer.append(posi_n)
-	print(answer)
+	return answer
+
+def find_distance(u,v):
+	x = u[0] - v[0]
+	y = u[1] - v[1]
+	dis = math.pow(x,2) + math.pow(y,2)
+	return math.sqrt(dis)
 	
+#m = [[0,0,0,0,0,0,0,0,1,1],
+#	 [0,0,0,0,0,1,0,0,0,0],
+#	 [1,0,1,0,0,0,1,0,0,0],
+#	 [1,0,0,1,0,0,0,0,0,1],
+#	 [0,1,1,1,1,0,0,0,0,1],
+#	 [0,1,0,0,0,0,0,0,0,1],
+#	 [0,0,0,0,0,0,0,1,0,0],
+#	 [1,0,0,0,0,0,0,0,1,0],
+#	 [1,0,0,1,0,0,0,1,0,1],
+#	 [1,1,0,0,0,0,0,0,0,0]]
+
 m = [[0,0,0,0,0,0,0,0,1,1],
 	 [0,0,0,0,0,1,0,0,0,0],
 	 [1,0,1,0,0,0,1,0,0,0],
 	 [1,0,0,1,0,0,0,0,0,1],
 	 [0,1,1,1,1,0,0,0,0,1],
-	 [0,1,0,0,0,0,0,0,0,1],
-	 [0,0,0,0,0,0,0,1,0,0],
-	 [1,0,0,0,0,0,0,0,1,0],
+	 [0,1,0,0,0,0,1,0,0,1],
+	 [0,0,0,0,0,0,0,0,0,0],
+	 [1,0,0,0,0,0,0,1,1,0],
 	 [1,0,0,1,0,0,0,1,0,1],
 	 [1,1,0,0,0,0,0,0,0,0]]
 
-all_posi(m,7,1)
-for row in m:
-	print(row)
+#answer = all_posi(m,3,10)
+#for row in m:
+#	print(row)
+	
+#print(answer)
+
+#for posi in answer:
+#	print(find_distance([3,4],posi))
+#print(find_distance([1,1],[3,3]))
+#print(find_distance([1,1],[1,3]))
+path = []
+current_post = [3,10,3]
+target_post = [3,4]
+path.append(current_post)
+
+for _ in range(6):
+	closets_dis = float('inf')
+	answer = all_posi(m,current_post[0],current_post[1])
+	print(answer)
+	for posi in answer:
+		current_dis = find_distance(target_post,posi)
+		if current_dis<closets_dis:
+			current_post = posi
+			closets_dis = current_dis
+	path.append(current_post)
+	for row in m:
+		print(row)
+print(path)
